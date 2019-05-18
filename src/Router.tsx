4a4351b4d6e58855e3516867788e679f22867f8e
Ryan Achten <ryanachten@gmail.com>
2019-05-18 15:23:30 +1200

    Add init router structure

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,26 @@
 import * as React from "react";
+import { ApolloProvider } from "react-apollo";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Index from "./pages/index";
 import Login from "./pages/login";
 import Test from "./pages/test";
 import withRoot from "./withRoot";
+import ApolloClient from "./ApolloClient";
 
 const Router = () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Route path="/" exact component={Index} />
-      <Route path="/login/" exact component={Login} />
-      <Route path="/history/" exact component={Test} />
-      <Route path="/exercises/" exact component={Test} />
-      <Route path="/exercises/new/" exact component={Test} />
-    </div>
-  </BrowserRouter>
+  <ApolloProvider client={ApolloClient}>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" exact component={Index} />
+        <Route path="/login/" exact component={Login} />
+        <Route path="/history/" exact component={Test} />
+        <Route path="/exercises/" exact component={Test} />
+        <Route path="/exercises/new/" exact component={Test} />
+      </div>
+    </BrowserRouter>
+  </ApolloProvider>
 );
 
 //  Apply MUI theme to root component to make accessible to children

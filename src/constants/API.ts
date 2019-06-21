aa4ee8970e920a6f6f53fe7382989f5447f93cf7
Ryan Achten <ryanachten@gmail.com>
2019-06-22 11:28:44 +1200

    Update README

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -3,6 +3,8 @@ const test = "https://robic-server.herokuapp.com/graphql";
 const prod = "https://robic-server.herokuapp.com/graphql";
 
 const getUrl = () => {
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
   switch (process.env.NODE_ENV) {
     case "development":
       return dev;

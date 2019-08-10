a4397b3462dc54f0eff128f42f1b9492f5c389a0
Ryan Achten <ryanachten@gmail.com>
2019-08-10 14:46:04 +1200

    Register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Router from "./Router";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Router />, document.querySelector("#root"));
+
+serviceWorker.register({});

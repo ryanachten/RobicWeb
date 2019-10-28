fd6449ff6d03194c0a67ca291fb68b33e881bad6
Ryan Achten <ryanachten@gmail.com>
2019-10-28 20:18:17 +1300

    Update Landing page styles

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,3 +1,2 @@
-export const LOGO_FONT = "'Hammersmith One', sans-serif";
 export const HEADER_FONT =
   "Arial Rounded MT Bold, Helvetica Rounded, Arial, sans-serif";
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,14 +5,25 @@ import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import createStyles from "@material-ui/core/styles/createStyles";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Classes } from "jss";
-import { LOGO_FONT } from "../constants/fonts";
-import { Button } from "@material-ui/core";
+import { Button, Divider } from "@material-ui/core";
 import routes from "../constants/routes";
+import { RobicLogo, PageTitle } from "../components";
+import { LIGHT_GRADIENT, PURPLE_GRADIENT } from "../constants/colors";
+import classnames from "../utils";
 
 const styles = (theme: Theme) =>
   createStyles({
-    robicLogo: {
-      fontFamily: LOGO_FONT
+    doneButton: {
+      marginTop: theme.spacing(2),
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.secondary.main
+    },
+    headerSection: {
+      alignItems: "center",
+      display: "flex",
+      flexFlow: "column",
+      marginBottom: theme.spacing(4),
+      padding: theme.spacing(4)
     },
     root: {
       alignItems: "center",
@@ -20,9 +31,27 @@ const styles = (theme: Theme) =>
       flexFlow: "column",
       paddingTop: theme.spacing(8)
     },
+    sectionContent: {
+      margin: "0 auto",
+      maxWidth: theme.breakpoints.values.sm,
+      padding: theme.spacing(4)
+    },
+    sectionDivider: {
+      margin: `${theme.spacing(2)}px 0`
+    },
+    sectionLight: {
+      background: LIGHT_GRADIENT,
+      color: theme.palette.primary.main
+    },
+    sectionPurple: {
+      background: PURPLE_GRADIENT,
+      color: theme.palette.common.white
+    },
     sectionRoot: {
-      marginBottom: theme.spacing(2),
-      maxWidth: theme.breakpoints.values.md
+      marginBottom: theme.spacing(2)
+    },
+    sectionWrapper: {
+      width: "100%"
     }
   });
 
@@ -37,15 +66,27 @@ type SectionProps = {
   classes: Classes;
   title: string;
   content: string;
+  variant: "purple" | "light";
 };
 
 const Section = withStyles(styles)(
-  ({ classes, title, content }: SectionProps) => (
-    <section className={classes.sectionRoot}>
-      <Typography variant="h6">{title}</Typography>
-      <Typography>{content}</Typography>
-    </section>
-  )
+  ({ classes, title, content, variant }: SectionProps) => {
+    const sectionClasses = classnames(
+      classes.sectionRoot,
+      variant === "purple" ? classes.sectionPurple : classes.sectionLight
+    );
+    return (
+      <div className={sectionClasses}>
+        <section className={classes.sectionContent}>
+          <Typography align="center" variant="h6">
+            {title}
+          </Typography>
+          <Divider className={classes.sectionDivider} />
+          <Typography>{content}</Typography>
+        </section>
+      </div>
+    );
+  }
 );
 
 class Landing extends React.Component<Props, State> {
@@ -62,35 +103,37 @@ class Landing extends React.Component<Props, State> {
     const { classes } = this.props;
     return (
       <main className={classes.root}>
-        <Typography className={classes.robicLogo} variant="h1">
-          robic
-        </Typography>
-        <article>
-          <section className={classes.sectionRoot}>
-            <Typography align="center" color="textSecondary" variant="h5">
-              exercise analytics to help monitor and improve your performance
-            </Typography>
-          </section>
+        <section className={classes.headerSection}>
+          <RobicLogo size="large" />
+          <PageTitle>Everyday exercise data</PageTitle>
+          <Typography variant="subtitle1">
+            Analytics to help monitor and improve your exercise performance
+          </Typography>
+          <Button
+            className={classes.doneButton}
+            variant="contained"
+            onClick={() => this.navigateToLogin()}
+          >
+            Get started!
+          </Button>
+        </section>
+        <article className={classes.sectionWrapper}>
           <Section
-            title="Create an exercise"
+            variant="purple"
+            title="manage exercises"
             content="Create an exercise tailored to your needs. Robic has support for a range of exercise types, including circuit and superset exercises. These can be assigned additional information, such as units and muscle groups for granular analysis."
           />
           <Section
-            title="Start an exercise"
+            variant="light"
+            title="record your effort"
             content="Simply select from the list of your exercises to get going. Robic allows you to time your exercise while adding set and repetition information. Previous exercise and personal best information are designed to push you to your limits."
           />
           <Section
-            title="Monitor progress"
+            variant="purple"
+            title="monitor progress"
             content="Over time, Robic will be able to provide you insight into areas you are excelling in, and areas which could do with some improvement. Analytics can also be viewed on a specific exercise or over a period of time."
           />
         </article>
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={() => this.navigateToLogin()}
-        >
-          Get started!
-        </Button>
       </main>
     );
   }
